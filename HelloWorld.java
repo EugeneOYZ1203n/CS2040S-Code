7d5bd55b93fdce2c3ec1a73c384ec38cb3d0c836
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-01-14 14:05:22 +0800

    PS1

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    static String name = "Eugene Oh";
+    static String fav_algorithm = "Quake Fast Inverse Square Root";
+    static String background_info = "I am having a fun time in NUS.";
+
+    public static void main(String[] args) {
+        System.out.println(
+            "My name is " 
+            + name 
+            + ". My favourite algorithm is " 
+            + fav_algorithm
+            + ". Some more info about me is "
+            + background_info +
+            ". The answers to 1a is 3125 and to 1b is The mystery function is calculating argA to the power of argB"
+        );
+    }
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleImage image = new SimpleImage("test", "mystery.bmp");
+        
+        int height = image.getImgHeight();
+        int width = image.getImgWidth();
+
+        for (int i = 0; i < height; i++ ){
+            for (int j = 0; j < width; j++ ) {
+                
+            }
+        }
+    }
+}
